5359aab9585afceef50c5c453471efb639c6cd1b
Gurwinder Singh <gurwinder@asritsolutions.com>
2021-07-11 02:46:03 -0700

    nutrition fix

--- src/components/Nutrition/NutritionForm.js
+++ src/components/Nutrition/NutritionForm.js
@@ -3,7 +3,7 @@ import { Button, Card, Container, IconButton } from '@material-ui/core';
 import trash2Fill from '@iconify/icons-eva/trash-2-fill';
 import React, { useEffect, useState } from 'react';
 import { useForm } from "react-hook-form";
-import { addNutrition } from '../../apiCalls/reportCalls';
+import { addNutrition, getNutrition } from '../../apiCalls/reportCalls';
 import { isLoggedIn } from "../../helpers/loginHelp"
 
 export default function NutritionForm(props) {
@@ -12,6 +12,34 @@ export default function NutritionForm(props) {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState("");
     const [array, setArray] = useState([0, 1]);
+
+    const [nutrition, setNutrition] = useState({});
+
+    useEffect(() => {
+        const token_ = isLoggedIn();
+        setLoading(true)
+        setNutrition([])
+        getNutrition(token_.token, token_.user.id).then(data => {
+            if (data && Array.isArray(data) && data.length > 0) {
+                const s = []
+                data[0].meals.forEach((e, index) => {
+                    s.push(index)
+                })
+                s.push(s.length)
+                setArray(s)
+                setNutrition(data[0])
+                setLoading(false)
+            } else {
+                // setError("Unable to get nutrition data")
+                setLoading(false)
+            }
+        })
+    }, [props])
+    if (loading) {
+        return "loading"
+    }
+    console.log(array)
+    console.log(nutrition)
 
     const submit = (e) => {
         const meals = []
@@ -43,13 +71,14 @@ export default function NutritionForm(props) {
             <Card>
                 <Container>
                     <h4 className="mt-2">Nutrition</h4>
-                    <form onSubmit={handleSubmit(submit)}>
+                    <form onSubmit={handleSubmit(submit)} >
                         <div>
                             <div className="row">
                                 <div className="col-lg-2 col-xl-1 col-sm-2">
                                     <div className="form-group ">
                                         <label className="text-muted">Wake Up</label>
-                                        <input className="form-control" type="text"{...register("wake_up", { required: true })} />
+                                        <input className="form-control"
+                                            type="text"{...register("wake_up", { required: true, value: nutrition.wake_up })} />
                                     </div>
                                 </div>
                                 <div className="col-lg-2 col-xl-2 col-sm-2">
@@ -88,62 +117,67 @@ export default function NutritionForm(props) {
                                 </div>
 
                             </div>
-                            {array.map((num, index) =>
-                                <div className="row" key={index}>
-                                    <div className="col-lg-2 col-xl-1 col-sm-2">
-                                        <div className="form-group ">
-                                            <label className="text-muted">Meal Time</label>
-                                            <input className="form-control" type="text" {...register(num + "_time", { required: true })} />
+                            {array.map((num, index) => {
+                                const meal = (nutrition.meals || [])[index] || {}
+                                return (
+                                    <div className="row" key={index}>
+                                        <div className="col-lg-2 col-xl-1 col-sm-2">
+                                            <div className="form-group ">
+                                                <label className="text-muted">Meal Time</label>
+                                                <input className="form-control"
+                                                    type="text" {...register(num + "_time", { required: true, value: meal.time })} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-lg-2 col-xl-2 col-sm-2">
-                                        <div className="form-group">
-                                            <label className="text-muted">Steak</label>
-                                            <input className="form-control" type="text"  {...register(num + "_steak", { required: true })} />
+                                        <div className="col-lg-2 col-xl-2 col-sm-2">
+                                            <div className="form-group">
+                                                <label className="text-muted">Steak</label>
+                                                <input className="form-control" type="text"  {...register(num + "_steak", { required: true })} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-lg-2 col-xl-1 col-sm-2">
-                                        <div className="form-group">
-                                            <label className="text-muted">Calories</label>
-                                            <input className="form-control" type="number" {...register(num + "_calories", { required: true })} />
+                                        <div className="col-lg-2 col-xl-1 col-sm-2">
+                                            <div className="form-group">
+                                                <label className="text-muted">Calories</label>
+                                                <input className="form-control" type="number" {...register(num + "_calories", { required: true })} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-lg-2 col-xl-1 col-sm-2">
-                                        <div className="form-group">
-                                            <label className="text-muted">Protein</label>
-                                            <input className="form-control" type="number"  {...register(num + "_protein", { required: true })} />
+                                        <div className="col-lg-2 col-xl-1 col-sm-2">
+                                            <div className="form-group">
+                                                <label className="text-muted">Protein</label>
+                                                <input className="form-control" type="number"  {...register(num + "_protein", { required: true })} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-lg-2 col-xl-2 col-sm-2">
-                                        <div className="form-group">
-                                            <label className="text-muted">Carbohydrates</label>
-                                            <input className="form-control" type="number"  {...register(num + "_carbohydrates", { required: true })} />
+                                        <div className="col-lg-2 col-xl-2 col-sm-2">
+                                            <div className="form-group">
+                                                <label className="text-muted">Carbohydrates</label>
+                                                <input className="form-control" type="number"  {...register(num + "_carbohydrates", { required: true })} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-lg-2 col-xl-1 col-sm-2">
-                                        <div className="form-group">
-                                            <label className="text-muted">Fats</label>
-                                            <input className="form-control" type="number"  {...register(num + "_fats", { required: true })} />
+                                        <div className="col-lg-2 col-xl-1 col-sm-2">
+                                            <div className="form-group">
+                                                <label className="text-muted">Fats</label>
+                                                <input className="form-control" type="number"  {...register(num + "_fats", { required: true })} />
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div className="col-lg-2 col-xl-3 col-sm-2">
-                                        <div className="form-group">
-                                            <label className="text-muted">Comment</label>
-                                            <input className="form-control" type="text"  {...register(num + "_comment", { required: true })} />
+                                        <div className="col-lg-2 col-xl-3 col-sm-2">
+                                            <div className="form-group">
+                                                <label className="text-muted">Comment</label>
+                                                <input className="form-control" type="text"  {...register(num + "_comment", { required: true })} />
+                                            </div>
                                         </div>
-                                    </div>
 
-                                    <div className="col-lg-3 col-xl-1 col-sm-4 pb-4">
-                                        <IconButton sx={{ mt: 2, color: 'text.primary' }}
-                                            onClick={() => {
-                                                const filterArray = array.filter((e) => e !== num)
-                                                setArray(filterArray)
-                                            }}
-                                            aria-label="delete">
-                                            <Icon icon={trash2Fill} />
-                                        </IconButton>
+                                        <div className="col-lg-3 col-xl-1 col-sm-4 pb-4">
+                                            <IconButton sx={{ mt: 2, color: 'text.primary' }}
+                                                onClick={() => {
+                                                    const filterArray = array.filter((e) => e !== num)
+                                                    setArray(filterArray)
+                                                }}
+                                                aria-label="delete">
+                                                <Icon icon={trash2Fill} />
+                                            </IconButton>
+                                        </div>
                                     </div>
-                                </div>
+                                )
+                            }
                             )}
                             {/* <div className="row">
                                     <h5 className="mt-4">Daily Totals</h5>
